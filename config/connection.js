261a221aa7705fb95cfe5c0ec0e6602614156493
James Holding <james.holding90@gmail.com>
2020-07-13 23:34:18 -0400

    config update.

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'hacktheplanet',
-        databse: 'todoagain_db'
+        password: 'password',
+        databse: 'burgers_db'
     });
 };
 
