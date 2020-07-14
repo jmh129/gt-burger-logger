abd025d2b90b4db84445f9066f33a803e662b299
James Holding <james.holding90@gmail.com>
2020-07-14 10:17:29 -0400

    imported cats orm to test.

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
   
 // Set up MySQL connection.
-const mysql = require("mysql");
-let connection;
+var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
+var connection;
+
+if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
@@ -11,12 +12,12 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     user: "root",
     password: "password",
-    database: "burgers_db",
-  });
+    database: "burgers_db"
+  })
 }
 
 // Make connection.
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
